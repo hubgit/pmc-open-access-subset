567b0f53ddd6b7ec283a89dfe0a4fdc048ed527f
Alf Eaton <alf@hubmed.org>
2013-07-18 12:12:11 +0100

    Update split.sh

--- split.sh
+++ split.sh
@@ -9,4 +9,4 @@ sudo chown ubuntu /data
 wget -e 'robots=off' --accept 'xml.gz' --level=1 -m http://europepmc.org/ftp/oa/
 
 mkdir /data/files
-find 'ukpmc.ac.uk/ftp/oa' -name '*.xml.gz' -print0 | xargs -0 zcat | awk -F, '$1 ~ /<!DOCTYPE/ { close("/data/files/article-"n".xml"); n++ }n{ f="/data/files/article-"n".xml"; print >f }'
+find 'europepmc.org/ftp/oa' -name '*.xml.gz' -print0 | xargs -0 zcat | awk -F, '$1 ~ /<!DOCTYPE/ { close("/data/files/article-"n".xml"); n++ }n{ f="/data/files/article-"n".xml"; print >f }'
