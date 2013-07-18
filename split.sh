13c043ac310edbf6561fc81ca50c166e7c1ca6be
Alf Eaton <alf@hubmed.org>
2013-07-18 12:44:59 +0100

    Update split.sh

--- split.sh
+++ split.sh
@@ -4,9 +4,13 @@
 # create 50GB volume and attach (/dev/sdf becomes /dev/vxdf)
 sudo mkfs.xfs /dev/xvdf
 sudo mkdir /data
+sudo mount /dev/xvdf /data
 sudo chown ubuntu /data
 
-wget -e 'robots=off' --accept 'xml.gz' --level=1 -m http://europepmc.org/ftp/oa/
+cd /data
 
-mkdir /data/files
-find 'europepmc.org/ftp/oa' -name '*.xml.gz' -print0 | xargs -0 zcat | awk -F, '$1 ~ /<!DOCTYPE/ { close("/data/files/article-"n".xml"); n++ }n{ f="/data/files/article-"n".xml"; print >f }'
+mkdir europepmc
+wget -e 'robots=off' --accept '.xml.gz' --recursive --level=1 --timestamping --no-parent --no-directories --continue --directory-prefix=europepmc http://europepmc.org/ftp/oa/
+
+mkdir articles
+find 'europepmc' -name '*.xml.gz' -print0 | xargs -0 zcat | awk -F, '$1 ~ /<!DOCTYPE/ { close("articles/article-"n".xml"); n++ }n{ f="articles/article-"n".xml"; print >f }'
